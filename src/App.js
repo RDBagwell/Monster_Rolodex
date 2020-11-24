efb7a34bd7864baf0a35c3b53e7cafc8db2a774f
Robert B <robertb@lifesciencepublishers.com>
2020-11-24 16:52:30 -0700

    Monster Rolodex

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, {Component} from 'react';
 import './App.css';
 import { CardList } from './components/Card-List/Card-List';
-import './components/Card-List/cradlist.css'
-
+import './components/Card-List/cradlist.css';
+import { SearchBox } from './components/Search-Box/Search-Box';
 class App extends Component {
   constructor(){
     super();
     this.state = {
-      monsters: []
+      monsters: [],
+      seachFeild: ''
     }
   }
   async componentDidMount(){
@@ -15,10 +16,20 @@ class App extends Component {
     const users = await res.json();
     this.setState({monsters: users});
   }
+
+  handleChange = (e)=> {
+    this.setState({seachFeild: e.target.value});
+  }
+
   render(){
+    const {monsters, seachFeild} = this.state;
+    const filteredMonsters = monsters.filter( monster => monster.name.toLowerCase().includes(seachFeild.toLowerCase()));
+
     return (
       <div className="App">
-        <CardList monsters={this.state.monsters} />
+        <h1>Monster Rolodex</h1>
+        <SearchBox placeholder="Search Monsters" handleChange={this.handleChange}/>
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
